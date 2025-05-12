7689d2865c1f86cb85f5c6c9482d51e39ed9621c
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-12 14:43:14 +0100

    feat(migration): simplify scheduling with direct Section-Schedule relationship
    - Removed SectionSchedule junction table
    - Added ScheduleId+TimeSlot to Section
    - Dropped seed data methods

---
+++ Migrations/20250512102310_Drop_SectionSchedule_update_sections.cs
@@ -0,0 +1,489 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EF010.CodeFirstMigration.Migrations
+{
+    /// <inheritdoc />
+    public partial class Drop_SectionSchedule_update_sections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SectionSchedules");
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 6, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 6, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 7, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 7, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 8, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 8, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 9, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 9, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 10, 9 });
+
+            migrationBuilder.DeleteData(
+                table: "Enrollments",
+                keyColumns: new[] { "SectionId", "StudentId" },
+                keyValues: new object[] { 10, 10 });
+
+            migrationBuilder.DeleteData(
+                table: "Schedules",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Schedules",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Schedules",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Schedules",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Schedules",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Sections",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Offices",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Offices",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Offices",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Offices",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Offices",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndTime",
+                table: "Sections",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScheduleId",
+                table: "Sections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartTime",
+                table: "Sections",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_ScheduleId",
+                table: "Sections",
+                column: "ScheduleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sections_Schedules_ScheduleId",
+                table: "Sections",
+                column: "ScheduleId",
+                principalTable: "Schedules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sections_Schedules_ScheduleId",
+                table: "Sections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_ScheduleId",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleId",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Sections");
+
+            migrationBuilder.CreateTable(
+                name: "SectionSchedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EndTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    ScheduleId = table.Column<int>(type: "int", nullable: false),
+                    SectionId = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectionSchedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SectionSchedules_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SectionSchedules_Sections_SectionId",
+                        column: x => x.SectionId,
+                        principalTable: "Sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "CourseName", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Mathmatics", 1000m },
+                    { 2, "Physics", 2000m },
+                    { 3, "Chemistry", 1500m },
+                    { 4, "Biology", 1200m },
+                    { 5, "CS-50", 3000m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Offices",
+                columns: new[] { "Id", "OfficeLocation", "OfficeName" },
+                values: new object[,]
+                {
+                    { 1, "building A", "Off_05" },
+                    { 2, "building B", "Off_12" },
+                    { 3, "Adminstration", "Off_32" },
+                    { 4, "IT Department", "Off_44" },
+                    { 5, "IT Department", "Off_43" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Schedules",
+                columns: new[] { "Id", "FRI", "MON", "SAT", "SUN", "THU", "TUE", "Title", "WED" },
+                values: new object[,]
+                {
+                    { 1, false, true, false, true, true, true, "Daily", true },
+                    { 2, false, false, false, true, true, true, "DayAfterDay", false },
+                    { 3, false, true, false, false, false, false, "Twice-a-Week", true },
+                    { 4, true, false, true, false, false, false, "Weekend", false },
+                    { 5, true, true, true, true, true, true, "Compact", true }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "FName", "LName" },
+                values: new object[,]
+                {
+                    { 1, "Fatima", "Ali" },
+                    { 2, "Noor", "Saleh" },
+                    { 3, "Omar", "Youssef" },
+                    { 4, "Huda", "Ahmed" },
+                    { 5, "Amira", "Tariq" },
+                    { 6, "Zainab", "Ismail" },
+                    { 7, "Yousef", "Farid" },
+                    { 8, "Layla", "Mustafa" },
+                    { 9, "Mohammed", "Adel" },
+                    { 10, "Samira", "Nabil" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Instructors",
+                columns: new[] { "Id", "FName", "LName", "OfficeId" },
+                values: new object[,]
+                {
+                    { 1, "Ahmed", "Abdullah", 1 },
+                    { 2, "Yasmeen", "Yasmeen", 2 },
+                    { 3, "Khalid", "Hassan", 3 },
+                    { 4, "Nadia", "Ali", 4 },
+                    { 5, "Ahmed", "Abdullah", 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Sections",
+                columns: new[] { "Id", "CourseId", "InstructorId", "SectionName" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, "S_MA1" },
+                    { 2, 1, 2, "S_MA2" },
+                    { 3, 2, 1, "S_PH1" },
+                    { 4, 2, 3, "S_PH2" },
+                    { 5, 3, 2, "S_CH1" },
+                    { 6, 3, 3, "S_CH2" },
+                    { 7, 4, 4, "S_BI1" },
+                    { 8, 4, 5, "S_BI2" },
+                    { 9, 5, 4, "S_CS1" },
+                    { 10, 5, 5, "S_CS2" },
+                    { 11, 5, 4, "S_CS3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Enrollments",
+                columns: new[] { "SectionId", "StudentId" },
+                values: new object[,]
+                {
+                    { 6, 1 },
+                    { 6, 2 },
+                    { 7, 3 },
+                    { 7, 4 },
+                    { 8, 5 },
+                    { 8, 6 },
+                    { 9, 7 },
+                    { 9, 8 },
+                    { 10, 9 },
+                    { 10, 10 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "SectionSchedules",
+                columns: new[] { "Id", "EndTime", "ScheduleId", "SectionId", "StartTime" },
+                values: new object[,]
+                {
+                    { 1, new TimeSpan(0, 10, 0, 0, 0), 1, 1, new TimeSpan(0, 8, 0, 0, 0) },
+                    { 2, new TimeSpan(0, 18, 0, 0, 0), 3, 2, new TimeSpan(0, 14, 0, 0, 0) },
+                    { 3, new TimeSpan(0, 15, 0, 0, 0), 4, 3, new TimeSpan(0, 10, 0, 0, 0) },
+                    { 4, new TimeSpan(0, 12, 0, 0, 0), 1, 4, new TimeSpan(0, 10, 0, 0, 0) },
+                    { 5, new TimeSpan(0, 18, 0, 0, 0), 1, 5, new TimeSpan(0, 16, 0, 0, 0) },
+                    { 6, new TimeSpan(0, 10, 0, 0, 0), 2, 6, new TimeSpan(0, 8, 0, 0, 0) },
+                    { 7, new TimeSpan(0, 14, 0, 0, 0), 3, 7, new TimeSpan(0, 11, 0, 0, 0) },
+                    { 8, new TimeSpan(0, 14, 0, 0, 0), 4, 8, new TimeSpan(0, 10, 0, 0, 0) },
+                    { 9, new TimeSpan(0, 18, 0, 0, 0), 4, 9, new TimeSpan(0, 16, 0, 0, 0) },
+                    { 10, new TimeSpan(0, 15, 0, 0, 0), 3, 10, new TimeSpan(0, 12, 0, 0, 0) },
+                    { 11, new TimeSpan(0, 11, 0, 0, 0), 5, 11, new TimeSpan(0, 9, 0, 0, 0) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectionSchedules_ScheduleId",
+                table: "SectionSchedules",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectionSchedules_SectionId",
+                table: "SectionSchedules",
+                column: "SectionId");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,10 +9,26 @@ namespace EF010.CodeFirstMigration
         {
             using (var context = new AppDbContext())
             {
-                foreach (var item in context.Sections.Include(x => x.Course))
+                var sections = context.Sections
+                    .Include(x => x.Course)
+                    .Include(x => x.Instructor)
+                    .Include(x => x.Schedule);
+                    //.Include(x => x.SectionSchedules);
+
+                Console.WriteLine("| Id |  Course      | Section | Instructor           | Schedule       | Time Slot     | SUN | MON | TUE | WED | THU | FRI | SAT |");
+                Console.WriteLine("|----|--------------|---------|----------------------|----------------|---------------|-----|-----|-----|-----|-----|-----|-----|");
+
+                foreach (var section in sections)
                 {
-                    Console.WriteLine($"Section: {item.SectionName}, " +
-                        $"Course {item.Course.CourseName}");
+                    string sunday = section.Schedule.SUN ? " x" : "";
+                    string monday = section.Schedule.MON ? " x" : "";
+                    string tuesday = section.Schedule.TUE ? " x" : "";
+                    string wednesday = section.Schedule.WED ? " x" : "";
+                    string thursday = section.Schedule.THU ? " x" : "";
+                    string friday = section.Schedule.FRI ? " x" : "";
+                    string saturday = section.Schedule.SAT ? " x" : "";
+
+                    Console.WriteLine($"| {section.Id.ToString().PadLeft(2, '0')} | {section.Course.CourseName,-12} | {section.SectionName,-7} | {(section.Instructor?.Name),-20} | {section.Schedule.Title,-14} | {section.TimeSlot,-9} | {sunday,-3} | {monday,-3} | {tuesday,-3} | {wednesday,-3} | {thursday,-3} | {friday,-3} | {saturday,-3} |");
                 }
             }
 
