dbc49d09a682365c35a6f47931870262ac7ef265
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-15 15:11:11 +0100

    Testing Query retrieval using Single, First, SingleOrDefault, FirstOrDefault

--- Program.cs
+++ Program.cs
@@ -9,14 +9,43 @@ namespace EF_Core_Project
         {
             using (var context = new AppDbContext())
             {
-                var courses = context.Courses;
+                //var courses = context.Courses;
+
+                //Console.WriteLine(courses.ToQueryString());
+
+                //foreach (var course in courses)
+                //    Console.WriteLine($"course name: {course.CourseName}, {course.HoursToComplete} hrs., {course.Price.ToString("C")}");
+
+                //var course = context.Courses.Single(x => x.Id == 1);
+
+                //Console.WriteLine($"course name: {course.CourseName}, {course.HoursToComplete} hrs., {course.Price.ToString("C")}");
+
+                //var course = context.Courses.Single(x => x.HoursToComplete == 25);
+
+                //Console.WriteLine($"{course.CourseName}, {course.Price.ToString("C")}");
+
+                // var course = context.Courses.First(x => x.HoursToComplete == 25);
+
+                // Console.WriteLine($"{course.CourseName}, {course.Price.ToString("C")}");
+
+                // var course = context.Courses.Single(x => x.HoursToComplete == 999);
+
+                // Console.WriteLine($"{course.CourseName}, {course.Price.ToString("C")}");
+
+                //var course = context.Courses.FirstOrDefault(x => x.HoursToComplete == 999);
+
+                //Console.WriteLine($"{course?.CourseName}, {course?.Price.ToString("C")}");
+
+                // var course = context.Courses.SingleOrDefault(x => x.HoursToComplete == 999);
+
+                // Console.WriteLine($"{course?.CourseName}, {course?.Price.ToString("C")}");
+
+                var courses = context.Courses.Where(x => x.Price > 3000);
+
                 Console.WriteLine(courses.ToQueryString());
 
-                Console.WriteLine("\n");
                 foreach (var course in courses)
-                {
-                    Console.WriteLine($"Course name: {course.CourseName}, {course.HoursToComplete} hrs, {course.Price.ToString("C")}");
-                }
+                    Console.WriteLine($"course name: {course.CourseName}, {course.HoursToComplete} hrs., {course.Price.ToString("C")}");
             }
 
             Console.ReadKey();
