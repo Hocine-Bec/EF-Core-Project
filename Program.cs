963b4e5e86ce1b3d3b9de798d9b4b883dce2317f
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-17 08:32:08 +0100

    Loading related data using explicit loading

--- Program.cs
+++ Program.cs
@@ -9,40 +9,20 @@ namespace EF_Core_Project
     {
         public static void Main(string[] args)
         {
-            //using (var context = new AppDbContext())
-            //{
-            //    var sectionQuery = context.Sections
-            //        .Include(x => x.Participants)
-            //        .Where(x => x.Id == 1);
-
-            //    Console.WriteLine(sectionQuery.ToQueryString());
-
-            //    var section = sectionQuery.FirstOrDefault();
-
-            //    Console.WriteLine($"\nSection Name: {section.SectionName}");
-
-            //    foreach (var part in section.Participants)
-            //    {
-            //        Console.WriteLine($"\n[{part.Id}] {part.FName} {part.LName}");
-            //    }
-            //}
-
             using (var context = new AppDbContext())
             {
-                var sectionQuery = context.Sections
-                    .Include(x => x.Instructor)
-                    .ThenInclude(x => x.Office)
-                    .Where(x => x.Id == 1);
+                var section = context.Sections
+                    .FirstOrDefault(x => x.Id == 1);
 
-                Console.WriteLine(sectionQuery.ToQueryString());
+                var query = context.Entry(section).Collection(x => x.Participants).Query();
+                Console.WriteLine(query.ToQueryString());
 
-                var section = sectionQuery.FirstOrDefault();
+                Console.WriteLine($"\nSection Name: {section.SectionName}");
 
-                Console.WriteLine($"\nSection Name: {section.SectionName}\n");
-
-                Console.WriteLine($"[{section.Id}] " +
-                    $"{section.Instructor.FName} {section.Instructor.LName} " +
-                    $"[{section.Instructor.Office.OfficeName}]");
+                foreach (var part in query)
+                {
+                    Console.WriteLine($"\n[{part.Id}] {part.FName} {part.LName}");
+                }
             }
 
             Console.ReadKey();
