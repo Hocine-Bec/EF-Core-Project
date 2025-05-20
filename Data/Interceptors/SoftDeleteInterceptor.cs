006b18261e41769840656fc6da195d82997f1321
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-20 11:58:47 +0100

    feat: create ISoftDelete interface and SoftDeleteInterceptor
    - Purpose: To learn soft delete implementation

---
+++ Data/Interceptors/SoftDeleteInterceptor.cs
@@ -0,0 +1,28 @@
+﻿using EF_Core_Project.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EF_Core_Project.Data.Interceptors
+{
+    public class SoftDeleteInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, 
+            InterceptionResult<int> result)
+        {
+            if (eventData.Context == null)
+                return result;
+
+            foreach (var entry in eventData.Context.ChangeTracker.Entries())
+            {
+                //if(entry != null || entry.State != EntityState.Modified || !(entry.Entity is ISoftDelete entity))
+                if (entry is not { State: EntityState.Deleted, Entity: ISoftDelete entity })
+                    continue;
+
+                entry.State = EntityState.Modified;
+                entity.Delete();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Interfaces/ISoftDelete.cs
@@ -0,0 +1,19 @@
+﻿namespace EF_Core_Project.Interfaces
+{
+    public interface ISoftDelete
+    {
+        bool IsDeleted { get; set; }
+        DateTime? DateDeleted { get; set; } 
+
+        public void Delete()
+        {
+            IsDeleted = true;
+            DateDeleted = DateTime.Now;
+        }
+        public void UndoDelete()
+        {
+            IsDeleted = false;
+            DateDeleted = null;
+        }
+    }
+}
